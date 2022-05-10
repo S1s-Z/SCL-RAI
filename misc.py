d1807257cc63232feaa940d9819e8e6a1a03c5d5
Shuzheng Si <sishuzheng@foxmail.com>
2022-05-10 23:47:23 +0800

    Update misc.py

--- misc.py
+++ misc.py
@@ -13,26 +13,6 @@ def iterative_support(func, query):
         return [iterative_support(func, i) for i in query]
     return func(query)
 
-def euclidean_dist(x, y):
-    """
-    Args:
-      x: pytorch Variable, with shape [m, d]
-      y: pytorch Variable, with shape [n, d]
-    Returns:
-      dist: pytorch Variable, with shape [m, n]
-    """
-
-    m, n = x.size(0), y.size(0)
-    # xx经过pow()方法对每单个数据进行二次方操作后，在axis=1 方向（横向，就是第一列向最后一列的方向）加和，此时xx的shape为(m, 1)，经过expand()方法，扩展n-1次，此时xx的shape为(m, n)
-    xx = torch.pow(x, 2).sum(1, keepdim=True).expand(m, n)
-    # yy会在最后进行转置的操作
-    yy = torch.pow(y, 2).sum(1, keepdim=True).expand(n, m).t()
-    dist = xx + yy
-    # torch.addmm(beta=1, input, alpha=1, mat1, mat2, out=None)，这行表示的意思是dist - 2 * x * yT
-    dist.addmm_(1, -2, x, y.t())
-    # clamp()函数可以限定dist内元素的最大最小范围，dist最后开方，得到样本之间的距离矩阵
-    dist = dist.clamp(min=1e-12).sqrt()  # for numerical stability
-    return 1/dist
 
 def fix_random_seed(state_val):
     random.seed(state_val)
@@ -115,12 +95,14 @@ def extract_json_data(file_path):
         dataset = json.load(fr)
     return dataset
 
+
 def sim_matrix(a, b, eps=1e-8):
     a_n, b_n = a.norm(dim=1)[:, None], b.norm(dim=1)[:, None]
     a_norm = a / torch.max(a_n, eps * torch.ones_like(a_n))
     b_norm = b / torch.max(b_n, eps * torch.ones_like(b_n))
     sim_mt = torch.mm(a_norm, b_norm.transpose(0, 1))
     return sim_mt
+
 
 def contrastive_loss(embedding, label, detach = False, temp=0.1, scale=100):
     if detach:
